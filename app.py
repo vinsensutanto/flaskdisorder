98697005d5f0c7a5c2d1a46fc2c0c58dd38a072a
vinsensutanto <vinsensutanto@gmail.com>
2025-03-04 22:24:05 +0700

    fix 2

--- app.py
+++ app.py
@@ -16,13 +16,16 @@ allowSelfSignedHttps(True)
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
+    if request.method == 'GET':
+        return jsonify({"message": "Use POST method to send data!"})
+
     data = request.json
     body = str.encode(json.dumps({"Inputs": {"input1": [data]}, "GlobalParameters": {}}))
 
     url = 'https://ac53101f-0356-4de0-bfb5-83276dd7e6ad.eastus2.azurecontainer.io/score'
-    api_key = 'OfdoyWgPCQmGmCiHshjrmFgXc4mPsh9E' 
+    api_key = 'OfdoyWgPCQmGmCiHshjrmFgXc4mPsh9E'
     headers = {'Content-Type': 'application/json', 'Authorization': ('Bearer ' + api_key)}
 
     req = urllib.request.Request(url, body, headers)
@@ -30,10 +33,8 @@ def predict():
     try:
         response = urllib.request.urlopen(req)
         result = json.loads(response.read())
-        result_data = result["Results"]["WebServiceOutput0"][0]
-        scored_label = result_data["Scored Labels"]
-        probabilities = {key.replace("Scored Probabilities_", ""): round(value * 100, 2) for key, value in result_data.items() if key.startswith("Scored Probabilities")}
-        return jsonify({"Scored Label": scored_label, "Probabilities": probabilities})
+        result_data = result["Results"]["WebServiceOutput0"]
+        return jsonify({"Results": {"WebServiceOutput0": result_data}})
     except urllib.error.HTTPError as error:
         return jsonify({"error": error.code, "message": error.read().decode("utf8", 'ignore')})
 
