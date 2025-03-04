8667558847093842aa3407bc9349c7d0acbae62c
vinsensutanto <vinsensutanto@gmail.com>
2025-03-04 19:14:51 +0700

    VINCI final boss fix 🔥

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
+@app.route('/predict', methods=['POST', 'GET'])
 
-def allowSelfSignedHttps(allowed=True):
+def allowSelfSignedHttps(allowed):
     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
         ssl._create_default_https_context = ssl._create_unverified_context
 
-allowSelfSignedHttps(True)  # Call this once outside
+allowSelfSignedHttps(True)
 
 @app.route('/')
 def index():
@@ -22,34 +22,38 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    logging.debug(f"Incoming Data: {request.data}")
+    logging.debug(f"Headers: {request.headers}")
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Invalid JSON Format"}), 400
+
+    body = str.encode(json.dumps({"Inputs": {"input1": [data]}, "GlobalParameters": {}}))
+
+    url = 'https://ac53101f-0356-4de0-bfb5-83276dd7e6ad.eastus2.azurecontainer.io/score'
+    api_key = 'OfdoyWgPCQmGmCiHshjrmFgXc4mPsh9E' 
+    headers = {'Content-Type': 'application/json', 'Authorization': ('Bearer ' + api_key)}
+
+    req = urllib.request.Request(url, body, headers)
+
     try:
-        data = request.get_json(force=True)
-        if not data:
-            return jsonify({"error": "Empty JSON payload"}), 400
-        
-        logging.debug(f"Incoming Data: {request.data}")
-        logging.debug(f"Headers: {request.headers}")
-
-        body = str.encode(json.dumps({"Inputs": {"input1": [data]}, "GlobalParameters": {}}))
-
-        url = 'https://ac53101f-0356-4de0-bfb5-83276dd7e6ad.eastus2.azurecontainer.io/score'
-        api_key = 'OfdoyWgPCQmGmCiHshjrmFgXc4mPsh9E'  
-        headers = {'Content-Type': 'application/json', 'Authorization': ('Bearer ' + api_key)}
-
-        req = urllib.request.Request(url, body, headers)
         response = urllib.request.urlopen(req)
         result = json.loads(response.read())
         result_data = result["Results"]["WebServiceOutput0"][0]
         scored_label = result_data["Scored Labels"]
         probabilities = {key.replace("Scored Probabilities_", ""): round(value * 100, 2) for key, value in result_data.items() if key.startswith("Scored Probabilities")}
-        
         return jsonify({"Scored Label": scored_label, "Probabilities": probabilities})
+    except urllib.error.HTTPError as error:
+        error_message = error.read().decode("utf8", 'ignore')
+        if "<!DOCTYPE" in error_message:
+            return jsonify({"error": error.code, "message": "Azure Internal Server Error. Check Model Deployment or API Key"}), 500
+        return jsonify({"error": error.code, "message": error_message}), 500
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
-from flask_cors import CORS
-CORS(app, resources={r"/*": {"origins": "*"}})  # Izinin semua origin
+if __name__ == '__main__':
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
     
 @app.errorhandler(Exception)
 def handle_exception(e):
